a956b218356395a8a6159d45e6352f321b05d7cf
LEESJ <happybin2022@gmail.com>
2023-01-20 21:33:46 +0900

    Initial commit

---
+++ src/main/java/com/wise_devTeam/cityOfDev/CityOfDevApplication.java
@@ -0,0 +1,13 @@
+package com.wise_devTeam.cityOfDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityOfDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityOfDevApplication.class, args);
+	}
+
+}
